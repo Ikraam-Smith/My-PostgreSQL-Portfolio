56607678ccbcb911b43ae5cfb4c7b11ab6092c6d
Ikraam-Smith <111156113+Ikraam-Smith@users.noreply.github.com>
2022-08-22 07:56:54 +0200

    Update 01_bookstore_data.sql

--- 01_bookstore_data.sql
+++ 01_bookstore_data.sql
@@ -1,7 +1,7 @@
 /* In my first postgresql porfolio project i will answer a few basic
 questions based on a bookstore dataset obtained on Kaggle */
 
--- First create the table
+--First create the table
 CREATE TABLE bookstore_data
 (title VARCHAR(250),
 book_category VARCHAR(50),
@@ -11,28 +11,28 @@ quantity_in_stock INTEGER);
 /* I then used the import function available on pgAdmin to import the csv file
 into the bookstore_data table that i just created */
 
--- Insert into the table information about a new book being sold
+--Insert into the table information about a new book being sold
 INSERT INTO bookstore_date
 (title,book_category,star_rating,price,quantity_in_stock)
 VALUES
 ('Beginners guide to SQL','Education','Five',24.99,25);
 
--- What are the different book categories?
+--What are the different book categories?
 SELECT DISTINCT (book_category)
 FROM bookstore_data;
 
--- How many books received five star ratings?
+--How many books received five star ratings?
 SELECT COUNT (title)
 FROM bookstore_data
 WHERE star_rating = 'Five';
 
--- What is the title and price of the most expensive book being sold
+--What is the title and price of the most expensive book being sold
 SELECT title, price
 FROM bookstore_data
 GROUP BY title, price
 ORDER BY price DESC
 LIMIT 1;
 
--- Which books only have 1 left in stock
+--Which books only have 1 left in stock
 SELECT title FROM bookstore_data
 WHERE quantity_in_stock = 1;
