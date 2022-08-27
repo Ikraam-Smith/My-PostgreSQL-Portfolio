ebfb47c88fda2b33ffd9dde7bb3fa72e6dfa3f8f
Ikraam-Smith <111156113+Ikraam-Smith@users.noreply.github.com>
2022-08-27 16:44:14 +0200

    Add files via upload

---
+++ 05_Employee_Subqueries.sql
@@ -0,0 +1,74 @@
+CREATE TABLE department(
+dept_id INTEGER ,
+dept_name VARCHAR(50) PRIMARY KEY,
+location VARCHAR(100));
+
+INSERT INTO department 
+VALUES 
+(1, 'Admin', 'Bangalore'),
+(2, 'HR', 'Bangalore'),
+(3, 'IT', 'Bangalore'),
+(4, 'Finance', 'Mumbai'),
+(5, 'Marketing', 'Bangalore'),
+(6, 'Sales', 'Mumbai');
+
+CREATE TABLE employee(
+emp_id INTEGER PRIMARY KEY,
+emp_name VARCHAR(50) NOT NULL,
+dept_name VARCHAR(50) NOT NULL REFERENCES department(dept_name),
+salary INTEGER);
+
+INSERT INTO employee 
+VALUES
+(101, 'Mohan', 'Admin', 4000),
+(102, 'Rajkumar', 'HR', 3000),
+(103, 'Akbar', 'IT', 4000),
+(104, 'Dorvin', 'Finance', 6500),
+(105, 'Rohit', 'HR', 3000),
+(106, 'Rajesh',  'Finance', 5000),
+(107, 'Preet', 'HR', 7000),
+(108, 'Maryam', 'Admin', 4000),
+(109, 'Sanjay', 'IT', 6500),
+(110, 'Vasudha', 'IT', 7000),
+(111, 'Melinda', 'IT', 8000),
+(112, 'Komal', 'IT', 10000),
+(113, 'Gautham', 'Admin', 2000),
+(114, 'Manisha', 'HR', 3000),
+(115, 'Chandni', 'IT', 4500),
+(116, 'Satya', 'Finance', 6500),
+(117, 'Adarsh', 'HR', 3500),
+(118, 'Tejaswi', 'Finance', 5500),
+(119, 'Cory', 'HR', 8000),
+(120, 'Monica', 'Admin', 5000),
+(121, 'Rosalin', 'IT', 6000),
+(122, 'Ibrahim', 'IT', 8000),
+(123, 'Vikram', 'IT', 8000),
+(124, 'Dheeraj', 'IT', 11000);
+
+CREATE TABLE employee_history(
+emp_id INTEGER REFERENCES employee(emp_id),
+emp_name VARCHAR(50) NOT NULL,
+dept_name VARCHAR(50) REFERENCES department(dept_name),
+salary INTEGER,
+location VARCHAR(100));
+
+CREATE TABLE sales(
+store_id INTEGER,
+store_name VARCHAR(50),
+product_name VARCHAR(50),
+quantity INTEGER,
+price INTEGER);
+
+INSERT INTO sales 
+VALUES
+(1, 'Apple Store 1','iPhone 13 Pro', 1, 1000),
+(1, 'Apple Store 1','MacBook pro 14', 3, 6000),
+(1, 'Apple Store 1','AirPods Pro', 2, 500),
+(2, 'Apple Store 2','iPhone 13 Pro', 2, 2000),
+(3, 'Apple Store 3','iPhone 12 Pro', 1, 750),
+(3, 'Apple Store 3','MacBook pro 14', 1, 2000),
+(3, 'Apple Store 3','MacBook Air', 4, 4400),
+(3, 'Apple Store 3','iPhone 13', 2, 1800),
+(3, 'Apple Store 3','AirPods Pro', 3, 750),
+(4, 'Apple Store 4','iPhone 12 Pro', 2, 1500),
+(4, 'Apple Store 4','MacBook pro 16', 1, 3500);
